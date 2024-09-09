6422c3fe4dac15a13573ebe5f922cf53d06602f0
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-09-09 17:52:21 -0500

    Update database.service.ts

--- backend/services/database.service.ts
+++ backend/services/database.service.ts
@@ -42,11 +42,12 @@ const masterKey = {
   keyRing: env.GCP_KEYRING,
   keyName: env.GCP_KEYNAME,
 };
-export async function createKey(altName?: string) {
+export async function createKey(altName: string) {
+  try {
   return await encryption.createDataKey("gcp", {
     masterKey,
     keyAltNames: altName ? [altName] : undefined,
-  });
+  });} catch{return (await encryption.getKeyByAltName(altName))?._id}
 }
 
 export async function connectToDatabase() {
