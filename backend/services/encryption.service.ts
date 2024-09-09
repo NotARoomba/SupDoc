c7b50bf31a6450059397cc33e80e9804921f9f79
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-09-09 11:18:03 -0500

    Update encryption.service.ts

--- backend/services/encryption.service.ts
+++ backend/services/encryption.service.ts
@@ -46,6 +46,7 @@ export default async function encryptionMiddleware(
     if (!req.body.key || !req.body.data) return res.sendStatus(401);
     const key = nodeRSA.decrypt(req.body.key, 'utf8');
     const data = CryptoJS.AES.decrypt(req.body.data, key);
+    console.log("DATA AND KEY", data, key)
     req.body = data.toString(CryptoJS.enc.Utf8);
     res.send = function (body: any) {
       const key = CryptoJS.lib.WordArray.random(256 / 8).toString();
