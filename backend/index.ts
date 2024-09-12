326b111b6d0b6961ea4b6a20c67695e06811becc
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-09-12 18:41:05 -0500

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import cors, { CorsOptions } from "cors";
-import { connectToDatabase } from "./services/database.service";
+import { connectToDatabase, env } from "./services/database.service";
 import encryptionMiddleware from "./services/encryption.service";
 import { verifyRouter } from "./routers/verify.router";
 import { patientsRouter } from "./routers/patients.router";
@@ -10,6 +10,7 @@ import { postsRouter } from "./routers/posts.router";
 import { doctorsRouter } from "./routers/doctors.router";
 import { usersRouter } from "./routers/users.router";
 import { factsRouter } from "./routers/facts.router";
+import NodeRSA from "node-rsa";
 
 export const app = express();
 const httpServer = createServer(app);
@@ -36,7 +37,30 @@ connectToDatabase()
     app.use("/", async (_req: Request, res: Response) => {
       res.status(200).send("You arent supposed to be here");
     });
-
+    
+app.response.send = function (body: any) {
+  const key = CryptoJS.SHA256(body).toString();
+  const encrypted = CryptoJS.AES.encrypt(
+    JSON.stringify(body),
+    key,
+  ).toString();
+  // need to check fot the public key of the user
+  // res.send = oldSend;
+  console.log("SEND")
+  console.log(this.req.headers.authorization)
+  return express.response.send({
+    key:
+      this.req.headers.authorization == env.LIMITED_AUTH
+        ? key
+        : (new NodeRSA(this.req.headers.authorization as string, "pkcs8-public", {
+            encryptionScheme: "pkcs1",
+            environment: "browser",
+          }))
+            .encrypt(key)
+            .toString(),
+    body: encrypted,
+  });
+};
     app.listen(port);
     console.log("Server started!");
     // app.listen(port, () => {
