2944c641dc39f0f59e48a0bc1ac8587fba4c5318
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-09-12 10:54:33 -0500

    utf 8

--- backend/services/encryption.service.ts
+++ backend/services/encryption.service.ts
@@ -16,7 +16,7 @@ export default async function encryptionMiddleware(
   if (!req.headers.authorization) return res.sendStatus(401);
   const obj = JSON.parse(CryptoJS.enc.Base64.parse(req.headers.authorization).toString(CryptoJS.enc.Utf8))
   const authKey = nodeRSA.decrypt(obj.key).toString();
-  const auth = CryptoJS.AES.decrypt(obj.data, authKey).toString();
+  const auth = CryptoJS.AES.decrypt(obj.data, authKey).toString(CryptoJS.enc.Utf8);
   console.log(auth)
   console.log(req.originalUrl)
   let publicKey: string = "none";
