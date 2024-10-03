4e904af0cd6d961f546688c5ab5566428f4b4bb9
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-10-03 17:55:39 -0500

    Update images.router.ts

--- backend/routers/images.router.ts
+++ backend/routers/images.router.ts
@@ -87,7 +87,7 @@ imagesRouter.post(
     if (!files || files.length == 0)
       return res
         .status(200)
-        .send({ status: STATUS_CODES.ERROR_UPLOADING_IMAGE });
+        .send(req.headers.authorization ? encrypt({ status: STATUS_CODES.ERROR_UPLOADING_IMAGE }, req.headers.authorization) : { status: STATUS_CODES.ERROR_UPLOADING_IMAGE });
     try {
       const urls = await Promise.all(
         files.map(async (image) => await uploadImageToStorage(image.path)),
@@ -96,13 +96,13 @@ imagesRouter.post(
       if (!urls || urls.every((url) => url === null))
         return res
           .status(200)
-          .send({ status: STATUS_CODES.ERROR_UPLOADING_IMAGE });
+          .send(req.headers.authorization ? encrypt({ status: STATUS_CODES.ERROR_UPLOADING_IMAGE }, req.headers.authorization) : { status: STATUS_CODES.ERROR_UPLOADING_IMAGE });
       if (urls)
-        return res.status(200).send({ urls, status: STATUS_CODES.SUCCESS });
-      else return res.status(200).send({ status: STATUS_CODES.GENERIC_ERROR });
+        return res.status(200).send(req.headers.authorization ? encrypt({ urls, status: STATUS_CODES.SUCCESS }, req.headers.authorization) : { urls, status: STATUS_CODES.SUCCESS });
+      else return res.status(200).send(req.headers.authorization ? encrypt({ status: STATUS_CODES.GENERIC_ERROR }, req.headers.authorization) : { status: STATUS_CODES.GENERIC_ERROR });
     } catch (error) {
       console.error("Error uploading image:", error);
-      res.status(200).send({ status: STATUS_CODES.GENERIC_ERROR });
+      res.status(200).send(req.headers.authorization ? encrypt({ status: STATUS_CODES.GENERIC_ERROR }, req.headers.authorization) : { status: STATUS_CODES.GENERIC_ERROR });
     }
   },
 );
