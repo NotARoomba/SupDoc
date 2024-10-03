73a9f2913ae908f09ce05732e440ca2e4d9c7175
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-10-03 12:45:32 -0500

    changes

--- web/src/tsx/pages/Home.tsx
+++ web/src/tsx/pages/Home.tsx
@@ -1,165 +1,173 @@
 import { motion } from "framer-motion";
 import "react-slideshow-image/dist/styles.css";
 import { Localizations } from "../utils/Localizations";
+import { Link } from "react-scroll";
 
 export default function Home() {
   const container = {
     hidden: { opacity: 0 },
-    show: {
+    visible: {
       opacity: 1,
       transition: {
-        staggerChildren: 0.5,
+        staggerChildren: 0.3,
       },
     },
   };
 
   const item = {
     hidden: { opacity: 0, y: 75 },
-    show: { opacity: 1, y: 0, transition: { duration: 0.75 } },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.75 } },
   };
 
   return (
     <div className="text-ivory bg-richer_black">
       {/* SupDoc */}
-      <div className="h-screen">
+      <div
+        id="home"
+        className="h-screen bg-gradient-to-b from-richer_black to-richer_black from-80%"
+      >
         <motion.div
           variants={container}
           initial="hidden"
-          animate="show"
-          className="h-full flex justify-center flex-col"
+          animate="visible"
+          className="h-screen flex pt-4 flex-col "
         >
-          <motion.div
-            variants={item}
-            className="w-fit mx-auto h-1/3 aspect-square"
-          >
-            <img className="aspect-square" src="/icon.png" />
+          <motion.div variants={item} className="mx-auto w-full md:w-1/2">
+            <img className="" src="/banner.png" />
           </motion.div>
-          <motion.p
-            variants={item}
-            className="text-7xl sm:text-8xl mx-auto font-bold"
-          >
-            SupDoc
-          </motion.p>
-          <motion.p
-            variants={item}
-            className="text-lg sm:text-xl mx-auto mt-6 text-center max-w-2xl px-4"
-          >
-            “SupDoc, Racionalizando* tu camino a la salud”
-          </motion.p>
-          <motion.p
-            variants={item}
-            className="text-powder_blue font-semibold text-lg mx-auto mt-4"
-          >
-            {Localizations.construction}
-          </motion.p>
+          <div className="flex md:flex-row flex-col-reverse w-full justify-center">
+            <div className="w-full mt-24 md:mt-0 md:w-1/2">
+              <motion.img
+                variants={item}
+                className="mx-auto md:ml-auto animate animate-slowBounce"
+                src="/splash-fade.png"
+              />
+            </div>
+            <div className="flex flex-col md:pt-36 w-full md:w-1/2 gap-y-8">
+              <motion.p
+                variants={item}
+                className="text-3xl text-ivory w-9/12 mx-auto md:w-96 font-bold text-center md:text-left md:mr-auto md:ml-0 mt-6 max-w-2xl"
+              >
+                {Localizations.simplificando}
+              </motion.p>
+              <motion.div
+                className="mx-auto md:ml-0  md:mr-auto"
+                variants={item}
+              >
+                <Link
+                  className="text-lg bg-midnight_green font-semibold text-center text-ivory hover:bg-oxforder_blue w-64 hover:text-ivory hover:cursor-pointer transition-all duration-300 p-2 px-4 rounded-xl"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  {Localizations.conoceProyecto}
+                </Link>
+              </motion.div>
+            </div>
+          </div>
         </motion.div>
       </div>
 
       {/* Sección 1 */}
-      <div className="h-screen bg-gray-900">
+      <div
+        className="h-screen w-full pt-8 md:pt-0 bg-gradient-to-b from-richer_black to-richer_black from-70%"
+        id="about"
+      >
         <motion.div
           variants={container}
           initial="hidden"
-          animate="show"
-          className="h-full flex items-center justify-center"
+          whileInView="visible"
+          className="h-full w-full flex md:flex-row flex-col items-center justify-center"
         >
-          <motion.div variants={item} className="flex items-center w-full max-w-4xl mx-auto">
-            {/* Imagen */}
-            <div className="w-1/2 flex flex-col justify-center items-center mt-8">
-              <img className="aspect-square w-1/2" src="/icon.png" />
-              {/* dato */}
-              <span className="text-gray-400 text-lg block text-center mt-20">
-                Las quejas ante la Superintendencia de Salud han incrementado hasta en un 12% en el último año.
-              </span>
-            </div>
-            {/* texto */}
-            <div className="w-1/2 text-right ml-8">
-              <motion.p
-                variants={item}
-                className="text-7xl sm:text-4xl font-bold text-center my-8"
-              >
-                La saturación de consultas en Colombia
-              </motion.p>
-              <motion.p
-                variants={item}
-                className="text-lg sm:text-xl mt-4 text-center max-w-2xl mx-auto"
-              >
-                SupDoc is a healthcare app designed to reduce the overload in specialist consultations and healthcare services. It provides a simple, reliable, and secure platform, allowing users to receive virtual medical attention completely anonymously.
-              </motion.p>
-              <motion.p
-                variants={item}
-                className="text-lg sm:text-xl mt-6 text-center max-w-2xl mx-auto"
-              >
-                SupDoc ensures effective communication between patients and specialists, delivering accurate and professional responses at all times.
-              </motion.p>
-            </div>
-          </motion.div>
+          {/* Imagen */}
+          <div className="w-full md:w-1/2 flex flex-col gap-y-4">
+            {/* dato */}
+            <motion.p
+              variants={item}
+              className="text-powder_blue w-11/12 mx-auto font-bold text-4xl block text-center md:text-right mt-20"
+            >
+              {Localizations.medicinaPreventiva}
+            </motion.p>
+            <motion.p
+              variants={item}
+              className="text-ivory/80 text-center md:text-right mx-auto text-lg w-11/12"
+            >
+              {Localizations.supdocDescripcion}
+            </motion.p>
+          </div>
+          {/* texto */}
+          <div className="w-full md:w-1/2 ">
+            <motion.img
+              variants={item}
+              className="ml-auto mt-24 md:mx-auto md:mt-0"
+              src="/home-fade.png"
+            />
+          </div>
         </motion.div>
       </div>
 
-
-
-
-      {/* Sección 2*/}
-      <div className="h-screen bg-gray-800">
-        <motion.div
-          variants={container}
-          initial="hidden"
-          animate="show"
-          className="h-full flex items-center justify-center"
-        >
-          <motion.div
-            variants={item}
-            className="w-full max-w-4xl mx-auto"
-          >
-            <motion.p
-              variants={item}
-              className="text-7xl sm:text-4xl font-bold text-center my-8"
-            >
-              Beneficios de SupDoc
-            </motion.p>
-            <div className="bg-gray-700 p-4 rounded-lg">
-              <table className="min-w-full border-collapse">
-                <thead>
-                  <tr>
-                    <th className="border-b border-gray-600 text-left text-lg font-semibold text-gray-300 px-4 py-2 text-center">
-                      Problemas de EPS
-                    </th>
-                    <th className="border-b border-gray-600 text-left text-lg font-semibold text-gray-300 px-4 py-5 text-center">
-                      Soluciones de SupDoc
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="border-b border-gray-600 text-center">
-                    <td className="px-4 py-2 text-gray-200">
-                      Largas esperas para citas.
-                    </td>
-                    <td className="px-4 py-2 text-gray-200">
-                      Atención médica virtual inmediata.
-                    </td>
-                  </tr>
-                  <tr className="border-b border-gray-600 text-center">
-                    <td className="px-4 py-2 text-gray-200">
-                      Limitaciones en el acceso a especialistas.
-                    </td>
-                    <td className="px-4 py-2 text-gray-200">
-                      Conexiones directas con médicos especializados.
-                    </td>
-                  </tr>
-                  <tr className="border-b border-gray-600 text-center">
-                    <td className="px-4 py-2 text-gray-200">
-                      Falta de seguimiento y comunicación.
-                    </td>
-                    <td className="px-4 py-2 text-gray-200">
-                      Plataforma segura para la comunicación constante.
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+      {/* FOOTER */}
+      <div className="w-full bg-richer_black/80 px-8">
+        <div className="md:w-7/12 mx-auto justify-center py-10">
+          <div className="flex flex-row">
+            <div className="justify-center mx-auto">
+              <p className="mb-2 font-bold text-4xl">SupDoc</p>{" "}
+              {/* Static Text */}
+              <p className="text-ivory">Barranquilla, Colombia</p>{" "}
+              {/* Static Text */}
+              <p className="text-ivory">Los Mayores Insociables</p>{" "}
+              {/* Static Text */}
+              <br />
+              <a
+                href="mailto:supdoc.ips@gmail.com"
+                className="my-9 underline text-ivory"
+              >
+                supdoc.ips@gmail.com
+              </a>
+              <br />
+              <a
+                href="https://github.com/NotARoomba/SupDoc"
+                className="my-9 underline text-ivory"
+              >
+                GitHub
+              </a>
+              <br />
+              <a
+                href="https://supdoc.org/policy_es.pdf"
+                className="my-9 underline text-ivory"
+              >
+                {Localizations.politica}
+              </a>
             </div>
-          </motion.div>
-        </motion.div>
+            <div className="justify-left mx-auto text-ivory">
+              <p className="text-2xl">{Localizations.enlaces}</p>
+              <Link
+                className="cursor-pointer underline"
+                activeClass="active"
+                to="home"
+                href="#home"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                {Localizations.inicio}
+              </Link>
+              <br />
+              <Link
+                className="cursor-pointer underline"
+                activeClass="active"
+                to="about"
+                href="#about"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                {Localizations.mision}
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
