44c547041d8036bfeb43e7e5ea56446f55aa5353
Nathan <70660308+NotARoomba@users.noreply.github.com>
2024-10-16 17:40:14 -0500

    whoops

--- backend/models/events.ts
+++ backend/models/events.ts
@@ -4,6 +4,7 @@ enum SupDocEvents {
   FETCH_POSTS = "fetch_posts",
   UPDATE_COMMENTS = "update_comments",
   UPDATE_FACT = "update_fact",
+  UPDATE_LANGUAGE = "update_language",
   POST_COMMENT = "post_comment",
   LIKE_COMMENT = "like_comment",
   LIKE_FACT = "like_fact",
